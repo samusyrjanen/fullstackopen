dc44e14165463bd1cbd12433ac7c8999fcf47f81
Samu Syrjänen <samu.syrjanen@gmail.com>
2023-01-10 17:51:54 +0200

    README.md

--- osa2/puhelinluettelo/README.md
+++ osa2/puhelinluettelo/README.md
@@ -1,3 +1,11 @@
+[https://fullstackopen-puhelinluettelo.fly.dev/](https://fullstackopen-puhelinluettelo.fly.dev/)
+
+
+
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -22,8 +22,10 @@ const deletePerson = (id) => {
   })
 }
 
-export default { 
+const requests = { 
   getAll,
   create,
   deletePerson,
 }
+
+export default requests
