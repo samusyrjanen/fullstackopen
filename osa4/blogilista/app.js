0fbe50890a62014a3940cf89fe741685987251f9
Samu Syrjänen <samu.syrjanen@gmail.com>
2023-01-11 14:34:08 +0200

    tehtävä 4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,22 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+mongoose.set('strictQuery', true)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
